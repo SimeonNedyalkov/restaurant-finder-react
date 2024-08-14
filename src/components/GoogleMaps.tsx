87a045d21524c9b24c756a2bde6497d4a44e9b93
SimeonNedyalkov <112097158+SimeonNedyalkov@users.noreply.github.com>
2024-08-14 07:42:48 +0300

    navbar fixed on top

--- src/components/GoogleMaps.tsx
+++ src/components/GoogleMaps.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
 
 const containerStyle = {
-  width: '100%',
+  width: '400px',
   height: '400px',
 };
 
