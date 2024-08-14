d6d93b14663612e8a0a5d175e99089ab70423b14
SimeonNedyalkov <112097158+SimeonNedyalkov@users.noreply.github.com>
2024-08-14 08:09:52 +0300

    router implemented and react-router-dom installed

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,17 @@ import Navigation from './components/Navbar'
 import GoogleMaps from "./components/GoogleMaps"
 const googleAPIkey = 'AIzaSyBFjjrOVcDKM0B5OFIWLhtbHc52Ai27pIs'
 const BASE_URL = `https://maps.googleapis.com/maps/api/js?key=${googleAPIkey}`
+import {BrowserRouter,Route,Routes} from 'react-router-dom'
+import About from "./components/About"
 function App() {
   return (
     <>
     <Navigation/>
-    <GoogleMaps apiKey={googleAPIkey}/>
+    <Routes>
+      <Route path="/about" element={<About/>}/>
+      <Route path="/restaurant-finder" element={<GoogleMaps apiKey={googleAPIkey}/>}></Route>
+    </Routes>
+    
     </>
   )
 }
