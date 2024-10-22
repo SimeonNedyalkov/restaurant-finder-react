a9644ddb8fccbf53fae90f988cb7b5dfc6726c3b
SimeonNedyalkov <simeon_sg@abv.bg>
2024-10-22 10:16:45 +0300

    change of text

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,13 +1,16 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Home: React.FC = () => {
   return (
     <div className="homeContainer">
       <h1 className="title">Welcome to Restaurant Finder</h1>
-      <p className="subtitle">Discover the best restaurants near you with just a few clicks.</p>
+      <p className="subtitle">
+        Discover the best restaurants in Varna with just a few clicks.
+      </p>
       <p className="description">
-        Our app helps you find a variety of dining options, whether you're looking for a quick bite or a fine dining experience.
+        Our app helps you find a variety of dining options, whether you're
+        looking for a quick bite or a fine dining experience.
       </p>
       <Link to="/restaurant-finder">
         <button className="button">Find Restaurants</button>
